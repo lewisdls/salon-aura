d38f12a43eacf0a8adf28fe16e2e51d9d4948937
Lewis Nomar <lewisdls21@hotmail.com>
2024-10-17 10:26:56 -0400

    Updating config

--- client/api/globalApi.jsx
+++ client/api/globalApi.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const apiKey = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
+const apiUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:1337/api",
+  baseURL: apiUrl,
   headers: {
     Authorization: `Bearer ${apiKey}`,
   },
