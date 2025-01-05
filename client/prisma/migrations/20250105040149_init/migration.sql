b9c2198d74852f895f9c2d8d36ba84e39c9b79d9
Lewis Nomar <lewisdls21@hotmail.com>
2025-01-05 00:15:44 -0400

    Changing from SQLite to PostgreSQL

--- client/prisma/migrations/20250105040149_init/migration.sql
+++ client/prisma/migrations/20250105040149_init/migration.sql
@@ -1,10 +1,12 @@
 -- CreateTable
 CREATE TABLE "Appointment" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "client_name" TEXT NOT NULL,
     "client_phone" TEXT NOT NULL,
     "date" TEXT NOT NULL,
     "time" TEXT NOT NULL,
     "service" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Appointment_pkey" PRIMARY KEY ("id")
 );
